5648ea970d53e24068718979bc9765f45d0a4434
Matheus8520 <matheusbatista6464@gmail.com>
2025-05-30 21:54:34 -0300

    Add files via upload

---
+++ Programacao em Python/H-PGPYT-N25350G/exercicios_aluno/Matheus_tarefa1.py
@@ -0,0 +1,29 @@
+# nome = input("Digite o nome do Aluno:")
+# nota1 = float(input("Digite a Primeira Nota:"))
+# nota2 = float(input("Digite a Segunda Nota:"))
+# media = (nota1+nota2) / 2 
+# if media>= 7:
+#     situacao = "Aprovado" 
+# elif media>= 5:
+#     situacao = "Recuperacao"
+# else:
+#     situacao = "Reprovado"
+
+# print(situacao)
+
+
+# nome =input("Seu Nome:") 
+# peso =float(input("Seu peso:"))
+# altura =float(input("Sua altura:"))
+
+# imc = peso / (altura * altura)
+# print(f"IMC{imc:2f}")
+# if imc < 18.5:print("magreza")
+# elif imc < 25:print("normal")
+# elif imc < 30:print("sobrepeso")
+# elif imc < 40:print("obesidade")
+# else: print("obesidede grave")
+
+total = input("Digite o valor da compra:")
+if total > 100:
+
