f45a5896905afc7dad4c27f056f13b0a1860c230
Matheus8520 <matheusbatista6464@gmail.com>
2025-06-03 19:22:35 -0300

    Update Matheus_tarefa1.py

--- Programacao em Python/H-PGPYT-N25350G/exercicios_aluno/Matheus_tarefa1.py
+++ Programacao em Python/H-PGPYT-N25350G/exercicios_aluno/Matheus_tarefa1.py
@@ -1,33 +1,34 @@
-# nome = input("Digite o nome do Aluno:")
-# nota1 = float(input("Digite a Primeira Nota:"))
-# nota2 = float(input("Digite a Segunda Nota:"))
-# media = (nota1+nota2) / 2 
-# if media>= 7:
-#     situacao = "Aprovado" 
-# elif media>= 5:
-#     situacao = "Recuperacao"
-# else:
-#     situacao = "Reprovado"
+nome = input("Digite o nome do Aluno:")
+nota1 = float(input("Digite a Primeira Nota:"))
+nota2 = float(input("Digite a Segunda Nota:"))
+media = (nota1+nota2) / 2 
+if media>= 7:
+    situacao = "Aprovado" 
+elif media>= 5:
+    situacao = "Recuperacao"
+else:
+    situacao = "Reprovado"
 
-# print(situacao)
+print(situacao)
 
 
-# nome =input("Seu Nome:") 
-# peso =float(input("Seu peso:"))
-# altura =float(input("Sua altura:"))
+nome =input("Seu Nome:") 
+peso =float(input("Seu peso:"))
+altura =float(input("Sua altura:"))
 
-# imc = peso / (altura * altura)
-# print(f"IMC{imc:2f}")
-# if imc < 18.5:print("magreza")
-# elif imc < 25:print("normal")
-# elif imc < 30:print("sobrepeso")
-# elif imc < 40:print("obesidade")
-# else: print("obesidede grave")
+imc = peso / (altura * altura)
+print(f"IMC{imc:2f}")
+if imc < 18.5:print("magreza")
+elif imc < 25:print("normal")
+elif imc < 30:print("sobrepeso")
+elif imc < 40:print("obesidade")
+else: print("obesidede grave")
 
 total = float(input("Digite total da compra:R$"))
-# if total>100: 
-#     total-=total* 0.1
-# print(f"Total de descontos(10%):R${total:.2f}")
+if total>100: 
+    total-=total* 0.1
+print(f"Total de descontos(10%):R${total:.2f}")
 
-# print(f"Total:RS{total:.2f}") 
+print(f"Total:RS{total:.2f}") 
+ 
 
