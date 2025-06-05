1fc2a02ab05f9fcc3a9a84ecb441eff922c52ab4
PriSaeki <priscilasaeki@gmail.com>
2025-06-04 21:34:47 -0300

    Add files via upload

---
+++ Dias da semana - Priscila.py
@@ -0,0 +1,40 @@
+# a=int(imput("Escolha 1, 2, 3"))
+# match a:
+#     case 1:
+#         print ("você escolheu um")
+#         case 2:
+#         print("você escolheu dois")
+#         case 3:
+#         print("você escolheu três")
+#         case_:
+#         print("opção errada")
+
+        #liste os dias da semana e caso seja final de semana, exiba se é final de semana
+
+# dias=int(input("Qual o dia da semana vc escolhe (Escolha 1 para segunda, 2 para terça, 3 para quarta, 4 para quinta, 5 para sexta e 6 para o final de semana) ?"))
+# match dias:
+#     case 1:
+#         print("Segunda-feira. Bora começar a semana!")    
+#     case 2:
+#         print("Terça-feira")
+#     case 3:
+#         print("Quarta-feira")
+#     case 4:
+#         print("Quinta-feira")
+#     case 5:
+#         print("Sexta-feira! Tá chegando o final de semana!")
+#     case 6:
+#         print("Final de semana!")
+
+# Laço de repetição
+contador=0
+while contador <3:
+    print(contador)
+    contador +=1
+
+#     #1. Com range()
+# for i in range (0,21, 2)
+
+
+
+        
