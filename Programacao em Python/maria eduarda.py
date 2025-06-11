83b4680d182ded17d10b1a8647860af341aa76cd
tech-guru24 <me4937672@gmail.com>
2025-06-10 22:01:37 -0300

    atividade 6 questões
    maria eduarda

---
+++ Programacao em Python/maria eduarda.py
@@ -0,0 +1,29 @@
+x=1
+y=3
+for t in range(x,y+1): 1,2,3
+print(t)
+
+x=15
+y=0
+t=3
+for t in range (y,x+1,t):
+    print(t)
+
+
+salada_de_frutas=("melancia","laranja","maça","banana","uva")
+for i in enumerate (salada_de_frutas):
+    print(i)
+    (0,maça)
+    (1,laranja)
+    (2,morango)
+    (3,uva)
+
+for i in ranger(0,201,11):
+    if i % 2 == 0:
+        print(i)
+
+animais=["gato","cachorro","passáro","tigre"]
+for animal in animais:
+    print("animal:",animal)
+
+    nomes=["ana","bruno","carlos"]
