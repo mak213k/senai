884e875f50dc97b7a893cfbc2b58aca2d467f781
Matheus8520 <matheusbatista6464@gmail.com>
2025-06-10 21:52:56 -0300

    Add files via upload

---
+++ Programacao em Python/H-PGPYT-N25350G/exercicios_aluno/matheus_tarefa03.py
@@ -0,0 +1,58 @@
+# x=1
+# y=20
+# for t in range(x,y+1):
+#     print(t)
+
+# x=15
+# y=0
+# t=3
+# for i in range(y,x+1,t):
+#     print(i)
+    
+# x={15,1,13}
+# for i in x:
+#     print(i)
+
+# salada_fruntas=((reversed)("melancia laranja maça mamao uva"))
+# for i in salada_fruntas:
+#     print(i)
+
+
+# salada_fruntas=[]
+# for i in enumerate(salada_fruntas)
+#     print(i)
+
+# salada_fruntas=['melancia','laranja','maça','mamao','uva']
+# etiqueta=[1,2,3,4,5]
+# for i,j  in zip (salada_fruntas,etiqueta):
+#         print(i,j)
+
+
+
+# 1.com range()
+# x=200
+# y=0
+# t=11
+# for i in range(y,x+1,t):
+#     print(i)
+
+# 2.com lista
+# lista_animal=["Animal:gato","Animal:cachorro","Animal:tigre",]
+# for i in lista_animal:
+#    print(i)
+
+# 3.com enumerate()
+# lista_nomes=("ana" ,"bruno","carlos")
+# for i in enumerate(lista_nomes):
+#     print(i) 
+
+
+# lista_nomes=((reversed)("ana bruno carlos"))
+# for i in lista_nomes:
+#     print(i)  
+# 
+
+ 
+# 4.com zip
+
+
