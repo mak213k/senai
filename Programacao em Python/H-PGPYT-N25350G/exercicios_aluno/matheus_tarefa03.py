41f219467fad85d3093e3f2baaec6e22914871e9
Matheus8520 <matheusbatista6464@gmail.com>
2025-06-11 19:23:02 -0300

    Update matheus_tarefa03.py

--- Programacao em Python/H-PGPYT-N25350G/exercicios_aluno/matheus_tarefa03.py
+++ Programacao em Python/H-PGPYT-N25350G/exercicios_aluno/matheus_tarefa03.py
@@ -1,58 +1,38 @@
-# x=1
-# y=20
-# for t in range(x,y+1):
-#     print(t)
+1.com range()
+x=200
+y=0
+t=11
+for i in range(y,x+1,t):
+    print(i)
 
-# x=15
-# y=0
-# t=3
-# for i in range(y,x+1,t):
-#     print(i)
-    
-# x={15,1,13}
-# for i in x:
-#     print(i)
+2.com lista
+lista_animal=["Animal:gato","Animal:cachorro","Animal:tigre",]
+for i in lista_animal:
+   print(i)
 
-# salada_fruntas=((reversed)("melancia laranja maça mamao uva"))
-# for i in salada_fruntas:
-#     print(i)
+3.com enumerate()
+lista_nomes=("ana" ,"bruno","carlos")
+for i in enumerate(lista_nomes):
+    print(i) 
 
 
-# salada_fruntas=[]
-# for i in enumerate(salada_fruntas)
-#     print(i)
+lista_nomes=((reversed)("ana bruno carlos"))
+for i in lista_nomes:
+    print(i)  
 
-# salada_fruntas=['melancia','laranja','maça','mamao','uva']
-# etiqueta=[1,2,3,4,5]
-# for i,j  in zip (salada_fruntas,etiqueta):
-#         print(i,j)
-
-
-
-# 1.com range()
-# x=200
-# y=0
-# t=11
-# for i in range(y,x+1,t):
-#     print(i)
-
-# 2.com lista
-# lista_animal=["Animal:gato","Animal:cachorro","Animal:tigre",]
-# for i in lista_animal:
-#    print(i)
-
-# 3.com enumerate()
-# lista_nomes=("ana" ,"bruno","carlos")
-# for i in enumerate(lista_nomes):
-#     print(i) 
-
-
-# lista_nomes=((reversed)("ana bruno carlos"))
-# for i in lista_nomes:
-#     print(i)  
-# 
 
  
-# 4.com zip
+4.com zip
+
+lista=["arroz","feijao","cafe","leite"]
+precos=[5.20, 4.50, 4.57, 5.3]
+for lista,precos in zip(lista,precos):
+       print(f"{lista}:R${precos:.2f}")
+
+5 com string
+
+nome=("Python é a melhor linguagem do mundo")
+for i in nome:
+    print(i)
 
 
