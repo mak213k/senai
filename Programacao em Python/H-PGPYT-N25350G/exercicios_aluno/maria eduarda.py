624b32cd7f94112aba49352edd19f89d7a236ef9
tech-guru24 <me4937672@gmail.com>
2025-06-11 22:00:22 -0300

    11/06

---
+++ Programacao em Python/H-PGPYT-N25350G/exercicios_aluno/maria eduarda.py
@@ -0,0 +1,32 @@
+a=3
+b=4
+def soma(a,b):
+    c=a+b
+    print(c)
+
+
+def subtraçao (a,b):
+        c=a-b
+        print(c) 
+        return
+
+resultado=subtraçao(a,b)
+
+def verificador_email (email):
+      if"@" in email and (com) in mail:
+            print("email válido")
+      else:
+            print("e-mail inválido")
+
+def entrada_de_ponto(vendas):
+      entrada=input("07:00")
+      saída= input ("16:00")
+
+      def gerar_relatoriio (vendas) :
+            total= sum (vendas)
+            medía= total/len (vendas)
+            print (f"media de vendas:R$ 5000")
+            if media> 5000:
+    print("meta atingida"):
+        else:
+    print("meta não atigida")
